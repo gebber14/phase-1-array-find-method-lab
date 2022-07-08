408a037256b7ed53c5437f38facb145192e2b020
Jason Rothgeb <jrothgeb@gmail.com>
2022-07-08 14:01:31 -0500

    commit tree 8c887b9ca334054cfddbb923c100058bb4c6e229

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {
+    const isWin = record.find(record => record.result ==="W"); 
+    console.log(isWin);
+     if (isWin === undefined) {
+       console.log(undefined);
+       return undefined;
+     } else if (isWin.result === "W") {
+       console.log(isWin.year);
+       return isWin.year;
+     }
+   }
